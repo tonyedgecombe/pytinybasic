a6af2638e594c1d2e8bd54b78ee9832bb24b04b5
Tony Edgecombe <tony.edgecombe@frogmorecs.com>
2011-05-30 14:54:58 +0100

    More work on tokeniser

--- tb.py
+++ tb.py
@@ -10,7 +10,10 @@ class Token:
     OPERATOR = 3
     MULTOPERATOR = 4
     COMMAND = 5
-
+    VARIABLE = 6
+    RELOP = 7
+    EOF = 8
+    
     type = UNKNOWN
     value = None
 
@@ -21,8 +24,25 @@ class Tokenizer:
         self.prog = prog
         self.pos = 0
 
+    def currentChar(self):
+        return self.prog[self.pos]
+
+    def nextChar(self):
+        if self.pos + 1 < len(self.prog):
+            return self.prog[self.pos + 1]
+
+        return None
+
+    def eatWhiteSpace(self):
+        while self.pos < len(self.prog) and self.currentChar().isspace():
+            self.pos += 1
+
     def getNextToken(self):
-        if self.currentChar().isdigit():
+        self.eatWhiteSpace()
+
+        if self.pos >= len(self.prog):
+            return self.getEOF()
+        elif self.currentChar().isdigit():
             return self.getNumberToken()
         elif self.currentChar() == '"':
             return self.getStringToken()
@@ -30,10 +50,14 @@ class Tokenizer:
             return self.getOperator()
         elif self.currentChar() == '*' or self.currentChar() == '/':
             return self.getMultOperator()
+        elif self.currentChar().isalpha() and self.nextChar() != None and self.nextChar().isalpha():
+            return self.getCommand()
+        elif self.currentChar().isalpha():
+            return self.getVariable()
+        elif self.currentChar() == '<' or self.currentChar() == '>':
+            return self.getRelativeOperator()
 
-
-        token = Token()
-        return token
+        return Token()
 
     def getNumberToken(self):
         token = Token()
@@ -80,11 +104,44 @@ class Tokenizer:
 
         return token
 
-    def currentChar(self):
-        return self.prog[self.pos]
+    def getCommand(self):
+        token = Token()
+        token.type = token.COMMAND
+        token.value = ''
 
-    def nextChar(self):
-        return self.prog[self.pos + 1]
+        while self.pos < len(self.prog) and self.currentChar().isalpha():
+            token.value += self.currentChar()
+            self.pos += 1        
+
+        return token
+
+    def getVariable(self):
+        token = Token()
+        token.type = token.VARIABLE
+        token.value = self.currentChar()
+
+        self.pos += 1
+
+        return token
+
+    def getRelativeOperator(self):
+        token = Token()
+        token.type = token.RELOP
+        token.value = self.currentChar()
+
+        if self.pos + 1 < len(self.prog) and (self.nextChar() == '=' or self.nextChar() == '>' or self.nextChar() == '<'):
+            token.value += self.nextChar()
+            self.pos += 1
+
+        self.pos += 1
+        
+        return token
+
+    def getEOF(self):
+        token = Token()
+        token.type = token.EOF
+
+        return token
 
 
 class TestTokeniser(TestCase):
@@ -177,5 +234,81 @@ class TestTokeniser(TestCase):
         self.assertEqual(token.NUMBER, token.type)
         self.assertEqual(3, token.value)
 
+
+    def test_command(self):
+        tokenizer = Tokenizer()
+        tokenizer.parse('PRINT 100')
+
+        token = tokenizer.getNextToken()
+        self.assertEqual(token.COMMAND, token.type)
+        self.assertEqual("PRINT", token.value)
+
+        token = tokenizer.getNextToken()
+        self.assertEqual(token.NUMBER, token.type)
+        self.assertEqual(100, token.value)
+
+    def test_variable(self):
+        tokenizer = Tokenizer()
+        tokenizer.parse('D')
+
+        token = tokenizer.getNextToken()
+        self.assertEqual(token.VARIABLE, token.type)
+        self.assertEqual('D', token.value)
+
+    def test_relop_lessthan(self):
+        tokenizer = Tokenizer()
+        tokenizer.parse('<')
+
+        token = tokenizer.getNextToken()
+        self.assertEqual(token.RELOP, token.type)
+        self.assertEqual('<', token.value)
+
+    def test_relop_lessthanorequals(self):
+        tokenizer = Tokenizer()
+        tokenizer.parse('<=')
+
+        token = tokenizer.getNextToken()
+        self.assertEqual(token.RELOP, token.type)
+        self.assertEqual('<=', token.value)
+
+    def test_relop_notequals(self):
+        tokenizer = Tokenizer()
+        tokenizer.parse('<>')
+
+        token = tokenizer.getNextToken()
+        self.assertEqual(token.RELOP, token.type)
+        self.assertEqual('<>', token.value)
+
+    def test_relop_greater(self):
+        tokenizer = Tokenizer()
+        tokenizer.parse('>')
+
+        token = tokenizer.getNextToken()
+        self.assertEqual(token.RELOP, token.type)
+        self.assertEqual('>', token.value)
+
+    def test_relop_greateroreqials(self):
+        tokenizer = Tokenizer()
+        tokenizer.parse('>=')
+
+        token = tokenizer.getNextToken()
+        self.assertEqual(token.RELOP, token.type)
+        self.assertEqual('>=', token.value)
+
+    def test_relop_notequals2(self):
+        tokenizer = Tokenizer()
+        tokenizer.parse('><')
+
+        token = tokenizer.getNextToken()
+        self.assertEqual(token.RELOP, token.type)
+        self.assertEqual('><', token.value)
+
+
+    def testeof(self):
+        tokenizer = Tokenizer()
+        tokenizer.parse('')
+
+        token = tokenizer.getNextToken()
+        self.assertEqual(token.EOF, token.type)
 
 
