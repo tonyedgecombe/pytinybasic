22542aadecf198f68b9799e5cdce83de00a3f22c
Tony Edgecombe <tony.edgecombe@frogmorecs.com>
2011-06-01 13:52:38 +0100

    Refactoring

--- interpreter.py
+++ interpreter.py
@@ -249,165 +249,151 @@ class Interpreter:
 
 
 class TestInterpreter(TestCase):
+    def setUp(self):
+        self.interpreter = Interpreter()
+
     def test_init(self):
-        interpreter = Interpreter()
-        self.assertEqual(26, len(interpreter.variables))
-        self.assertEqual(0, interpreter.variables['A'])
+        self.assertEqual(26, len(self.interpreter.variables))
+        self.assertEqual(0, self.interpreter.variables['A'])
 
     def test_set_line(self):
-        interpreter = Interpreter()
-        interpreter.interpret_line('10 LET A = 100')
+        self.interpreter.interpret_line('10 LET A = 100')
 
-        self.assertEqual(1, len(interpreter.lines))
-        self.assertEqual({10:' LET A = 100'}, interpreter.lines)
+        self.assertEqual(1, len(self.interpreter.lines))
+        self.assertEqual({10:' LET A = 100'}, self.interpreter.lines)
 
-        interpreter.interpret_line('20 PRINT A')
-        self.assertEqual(2, len(interpreter.lines))
-        self.assertEqual(' PRINT A', interpreter.lines[20])
+        self.interpreter.interpret_line('20 PRINT A')
+        self.assertEqual(2, len(self.interpreter.lines))
+        self.assertEqual(' PRINT A', self.interpreter.lines[20])
 
     def test_let(self):
-        interpreter = Interpreter()
-        interpreter.run_line('LET B = 123 + 2')
-        self.assertEqual(125, interpreter.variables['B'])
+        self.interpreter.run_line('LET B = 123 + 2')
+        self.assertEqual(125, self.interpreter.variables['B'])
 
     def test_if(self):
-        interpreter = Interpreter()
+        self.interpreter.run_line('IF 2 < 1 THEN LET C = 100')
+        self.assertEqual(0, self.interpreter.variables['C'])
 
-        interpreter.run_line('IF 2 < 1 THEN LET C = 100')
-        self.assertEqual(0, interpreter.variables['C'])
+        self.interpreter.run_line('IF 2 > 1 THEN LET A = 100')
+        self.assertEqual(100, self.interpreter.variables['A'])
 
-        interpreter.run_line('IF 2 > 1 THEN LET A = 100')
-        self.assertEqual(100, interpreter.variables['A'])
-
-        interpreter.run_line('IF 1 THEN LET B = 200')
-        self.assertEqual(200, interpreter.variables['B'])
+        self.interpreter.run_line('IF 1 THEN LET B = 200')
+        self.assertEqual(200, self.interpreter.variables['B'])
 
     def test_match_var_list(self):
         tokenizer = Tokenizer()
         tokenizer.parse('A, B, C')
-        interpreter = Interpreter()
 
-        self.assertEqual(['A', 'B', 'C'], interpreter.match_var_list(tokenizer))
+        self.assertEqual(['A', 'B', 'C'], self.interpreter.match_var_list(tokenizer))
 
 
     def test_match_relop(self):
         tokenizer = Tokenizer()
-        interpreter = Interpreter()
 
         tokenizer.parse("2 > 1")
-        self.assertEqual(1, interpreter.match_relop(tokenizer))
+        self.assertEqual(1, self.interpreter.match_relop(tokenizer))
 
         tokenizer.parse("2 < 1")
-        self.assertEqual(0, interpreter.match_relop(tokenizer))
+        self.assertEqual(0, self.interpreter.match_relop(tokenizer))
         
         tokenizer.parse("2 <= 2")
-        self.assertEqual(1, interpreter.match_relop(tokenizer))
+        self.assertEqual(1, self.interpreter.match_relop(tokenizer))
 
         tokenizer.parse("2 <= 1")
-        self.assertEqual(0, interpreter.match_relop(tokenizer))
+        self.assertEqual(0, self.interpreter.match_relop(tokenizer))
 
         tokenizer.parse("2 >= 2")
-        self.assertEqual(1, interpreter.match_relop(tokenizer))
+        self.assertEqual(1, self.interpreter.match_relop(tokenizer))
 
         tokenizer.parse("2 >= 3")
-        self.assertEqual(0, interpreter.match_relop(tokenizer))
+        self.assertEqual(0, self.interpreter.match_relop(tokenizer))
 
         tokenizer.parse("2 = 2")
-        self.assertEqual(1, interpreter.match_relop(tokenizer))
+        self.assertEqual(1, self.interpreter.match_relop(tokenizer))
 
         tokenizer.parse("2 = 3")
-        self.assertEqual(0, interpreter.match_relop(tokenizer))
+        self.assertEqual(0, self.interpreter.match_relop(tokenizer))
 
         tokenizer.parse("2 <> 3")
-        self.assertEqual(1, interpreter.match_relop(tokenizer))
+        self.assertEqual(1, self.interpreter.match_relop(tokenizer))
 
         tokenizer.parse("2 <> 2")
-        self.assertEqual(0, interpreter.match_relop(tokenizer))
+        self.assertEqual(0, self.interpreter.match_relop(tokenizer))
 
         tokenizer.parse("2 >< 3")
-        self.assertEqual(1, interpreter.match_relop(tokenizer))
+        self.assertEqual(1, self.interpreter.match_relop(tokenizer))
 
         tokenizer.parse("2 >< 2")
-        self.assertEqual(0, interpreter.match_relop(tokenizer))
+        self.assertEqual(0, self.interpreter.match_relop(tokenizer))
 
 
     def test_expression_list(self):
         tokenizer = Tokenizer()
         tokenizer.parse('2+3+2*2, 1+2, 3, "abcd"')
-        interpreter = Interpreter()
 
-        self.assertEqual([9,3,3, 'abcd'], interpreter.match_expression_list(tokenizer))
+        self.assertEqual([9,3,3, 'abcd'], self.interpreter.match_expression_list(tokenizer))
 
 
     def test_expression(self):
         tokenizer = Tokenizer()
         tokenizer.parse('2+3+2*2')
-        interpreter = Interpreter()
 
-        self.assertEqual(9, interpreter.match_expression(tokenizer))
+        self.assertEqual(9, self.interpreter.match_expression(tokenizer))
 
     def test_match_term(self):
         tokenizer = Tokenizer()
         tokenizer.parse('2*3*4')
-        interpreter = Interpreter()
 
-        self.assertEqual(24, interpreter.match_term(tokenizer))
+        self.assertEqual(24, self.interpreter.match_term(tokenizer))
 
     def test_match_factor(self):
         tokenizer = Tokenizer()
         tokenizer.parse('123')
-        interpreter = Interpreter()
 
-        self.assertEqual(123, interpreter.match_factor(tokenizer))
+        self.assertEqual(123, self.interpreter.match_factor(tokenizer))
 
-        interpreter.variables['A'] = 456
+        self.interpreter.variables['A'] = 456
         tokenizer.parse('A')
-        self.assertEqual(456, interpreter.match_factor(tokenizer))
+        self.assertEqual(456, self.interpreter.match_factor(tokenizer))
 
 
     def test_run_program(self):
-        interpreter = Interpreter()
+        self.interpreter.lines[10] = 'LET A = 10'
+        self.interpreter.lines[20] = 'LET B = 20'
+        self.interpreter.lines[30] = 'LET C = A + B'
+        self.interpreter.lines[40] = 'END'
+        self.interpreter.lines[50] = 'LET C = 5'
 
-        interpreter.lines[10] = 'LET A = 10'
-        interpreter.lines[20] = 'LET B = 20'
-        interpreter.lines[30] = 'LET C = A + B'
-        interpreter.lines[40] = 'END'
-        interpreter.lines[50] = 'LET C = 5'
+        self.interpreter.run_program()
 
-        interpreter.run_program()
-
-        self.assertEqual(30, interpreter.variables['C'])
+        self.assertEqual(30, self.interpreter.variables['C'])
 
     def test_goto(self):
-        interpreter = Interpreter()
+        self.interpreter.lines[10] = 'LET M = 1'
+        self.interpreter.lines[20] = 'LET I = 0'
 
-        interpreter.lines[10] = 'LET M = 1'
-        interpreter.lines[20] = 'LET I = 0'
+        self.interpreter.lines[30] = 'LET M = M * 2'
+        self.interpreter.lines[40] = 'LET I = I + 1'
+        self.interpreter.lines[50] = 'IF I < 8 THEN GOTO 30'
 
-        interpreter.lines[30] = 'LET M = M * 2'
-        interpreter.lines[40] = 'LET I = I + 1'
-        interpreter.lines[50] = 'IF I < 8 THEN GOTO 30'
+        self.interpreter.run_program()
 
-        interpreter.run_program()
-
-        self.assertEqual(256, interpreter.variables['M'])
+        self.assertEqual(256, self.interpreter.variables['M'])
 
     def test_gosub(self):
-        interpreter = Interpreter()
+        self.interpreter.lines[10] = 'LET M = 1'
+        self.interpreter.lines[20] = 'LET I = 0'
 
-        interpreter.lines[10] = 'LET M = 1'
-        interpreter.lines[20] = 'LET I = 0'
+        self.interpreter.lines[30] = 'GOSUB 70'
+        self.interpreter.lines[40] = 'LET I = I + 1'
+        self.interpreter.lines[50] = 'IF I < 8 THEN GOTO 30'
+        self.interpreter.lines[60] = 'END'
+        self.interpreter.lines[70] = 'LET M = M * 2'
+        self.interpreter.lines[80] = 'RETURN'
 
-        interpreter.lines[30] = 'GOSUB 70'
-        interpreter.lines[40] = 'LET I = I + 1'
-        interpreter.lines[50] = 'IF I < 8 THEN GOTO 30'
-        interpreter.lines[60] = 'END'
-        interpreter.lines[70] = 'LET M = M * 2'
-        interpreter.lines[80] = 'RETURN'
+        self.interpreter.run_program()
 
-        interpreter.run_program()
-
-        self.assertEqual(256, interpreter.variables['M'])
+        self.assertEqual(256, self.interpreter.variables['M'])
 
 if __name__ == '__main__':
     print('Tiny Basic in Python')
